824a2007d365a5349850f573764ef10848f00c78
barakos404 <53255540+barakos404@users.noreply.github.com>
2020-02-26 11:12:11 +0200

    Update freeipa-service-password.sh

--- freeipa-service-password.sh
+++ freeipa-service-password.sh
@@ -1,7 +1,3 @@
-Created service radius/jfeufipa01.jfrog.internal
-Service Bind DN: krbprincipalname=radius/jfeufipa01.jfrog.internal@JFROG.INTERNAL,cn=services,cn=accounts,dc=jfrog,dc=internal
-Enabled login with generated password: nAkEk4jGYRyd10KMoq@Pm0K)ik&
-[ec2-user@jfeufipa01 ~]$
 
 
 API KEY ==
